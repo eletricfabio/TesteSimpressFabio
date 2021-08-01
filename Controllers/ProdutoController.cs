724f81f336ce35dc95c20c41d1fa322e7c7dbb7c
fabio.vieira@getinfo.tech <tgfzEhjR>
2021-08-01 20:19:25 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TesteSimpressFabio.Models;
+
+namespace TesteSimpressFabio.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private PRODUTOSTOREEntities db = new PRODUTOSTOREEntities();
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            var produto = db.Produto.Include(p => p.CategoriaProduto);
+            return View(db.Produto.ToList());
+        }
+
+        // GET: Produto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // GET: Produto/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProdutoCategoriaID = new SelectList(db.CategoriaProduto, "CategoriaID", "CategoriaNome");
+            return View();
+        }
+
+        // POST: Produto/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProdutoID,ProdutoNome,ProdutoDescricao,ProdutoAtivo,ProdutoPerecivel,ProdutoCategoriaID")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Produto.Add(produto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProdutoCategoriaID = new SelectList(db.CategoriaProduto, "CategoriaID", "CategoriaNome", produto.ProdutoCategoriaID);
+            return View(produto);
+        }
+
+        // GET: Produto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProdutoCategoriaID = new SelectList(db.CategoriaProduto, "CategoriaID", "CategoriaNome", produto.ProdutoCategoriaID);
+            return View(produto);
+        }
+
+        // POST: Produto/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProdutoID,ProdutoNome,ProdutoDescricao,ProdutoAtivo,ProdutoPerecivel,ProdutoCategoriaID")] Produto produto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(produto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProdutoCategoriaID = new SelectList(db.CategoriaProduto, "CategoriaID", "CategoriaNome", produto.ProdutoCategoriaID);
+            return View(produto);
+        }
+
+        // GET: Produto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Produto produto = db.Produto.Find(id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // POST: Produto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Produto produto = db.Produto.Find(id);
+            db.Produto.Remove(produto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TesteSimpressFabio.MvcApplication" Language="C#" %>
